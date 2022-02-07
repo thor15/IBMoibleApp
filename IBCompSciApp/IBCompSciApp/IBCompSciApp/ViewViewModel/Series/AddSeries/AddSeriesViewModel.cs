edd4647e73d272a40ee05e3983db87f4c86fe094
Brittan Samford <brittansamford@yahoo.com>
2022-02-07 11:29:06 -0600

    Delete

--- IBCompSciApp/IBCompSciApp/IBCompSciApp/ViewViewModel/Series/AddSeries/AddSeriesViewModel.cs
+++ IBCompSciApp/IBCompSciApp/IBCompSciApp/ViewViewModel/Series/AddSeries/AddSeriesViewModel.cs
@@ -66,7 +66,7 @@ namespace IBCompSciApp.ViewViewModel.Series.AddSeries
 
                 foreach (BookInformation book in apiData.docs)
                 { 
-                    if (book != null && CheckAuthorKey(book, "OL79034A")
+                    if (book != null && CheckAuthorKey(book, "OL79034A"))
                     {
                         BookInfo.Add(book);
                     }
